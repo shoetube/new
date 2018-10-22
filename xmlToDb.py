02c57c06a4b697dfb5cb0dfe00b50e78212970ea
shoetube <shoetube@hotmail.com>
2018-10-21 20:04:56 -0700

    convert xml file to list of dicts

--- xmlToDb.py
+++ xmlToDb.py
@@ -8,6 +8,7 @@ inputFile = open('animelist.xml')
 inputStr = inputFile.read()
 inputFile.close()
 
+# splits lines accross a list called l while keeping end of line character
 l = inputStr.splitlines(True)
 
 # delete head and tail of file
@@ -16,7 +17,7 @@ for i in range(deleteHead):
 for i in range(deleteTail):
   l.pop(-1)
 
-# recombines list to string
+# recombines list to string (possible future update: split on ('\t'*3 + '</'))
 tempStr = ''
 tempStr = tempStr.join(l)
 l = tempStr.split('</anime>')
@@ -42,7 +43,6 @@ while l.count([]):
   l.remove([])
 
 # removes end tags
-
 for i in range(len(l)):
   subFor = range(len(l[i])) 
   for j in subFor:
@@ -50,13 +50,57 @@ for i in range(len(l)):
     if sSlice > 1:
       l[i][j] = l[i][j].replace(l[i][j][sSlice:],'')
 
+# eliminate cdata enclosure
+s1 = '<![CDATA['
+s2 = ']]>'
+for i in l:
+  for j in i:
+    start1 = j.find(s1)
+    start2 = j.find(s2) 
+    if start1 > -1:
+      end1   = start1 + len(s1)
+      end2   = start2 + len(s2)
+      index = i.index(j)
+      i[index] = i[index].replace(s1, '')                                              
+      i[index] = i[index].replace(s2, '')
+
+# removes '_' and '<' characters
+for i in range(len(l)):
+  for j in range(len(l[i])):
+    l[i][j] = l[i][j].replace('_',' ')
+    l[i][j] = l[i][j].replace('<','')
+
+# splits sublist on '>' thus separating key and items
+for i in range(len(l)):
+  for j in range(len(l[i])):
+    l[i][j] = l[i][j].split('>')
+    
+# removes items with no value associated
+for i in range(len(l)):
+  j = 0
+  while j < len(l[i]):
+    if l[i][j][1] == '':
+      l[i].pop(j)
+      continue
+    j+=1
+
+list = []
+for i in range(len(l)):
+  list.append({})
+  for j in range(len(l[i])):
+    list[i].update({l[i][j][0]:l[i][j][1]})
+
+
+"""
 # Contents of output
 oString = ''
 for i in l:
   for j in i:
-    oString += j+'\n'
+    for k in j:
+      oString += k +'\n'
 
 # Outputs results to file
 outputFile = open('output.txt', 'w')
 outputFile.write(oString)
 outputFile.close()
+"""
